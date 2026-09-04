e0826496f3dc97c76ba63c742bfc4caa2a87857c
Michel Albuquerque <michel_albuquerque@id.uff.br>
2026-09-04 18:25:34 -0300

    Inicia plataforma operacional institucional

---
+++ supabase_config.js
@@ -0,0 +1,7 @@
+/* GERADO AUTOMATICAMENTE - configuração pública do Supabase.
+   Pode ser publicado no GitHub. NÃO contém Secret key. */
+window.SUPABASE_CONFIG = Object.freeze({
+  url: "https://dxgaylieybxpdipvzbog.supabase.co",
+  publishableKey: "sb_publishable_bE9KOTCn40C-5L_WfFIrsQ_m6LW6Mlj",
+  projectRef: "dxgaylieybxpdipvzbog"
+});
